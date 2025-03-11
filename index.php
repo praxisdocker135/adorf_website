ac7bc0621140e5789781ccee42ea264000854a89
CaptivatingRedbull <Marlon.schoeffend@icloud.com>
2025-03-11 21:06:05 +0100

    bla

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landratsamt Ansbach</title>
-    <!-- Zentrale CSS-Datei -->
+    <!-- Deine zentrale CSS-Datei -->
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
